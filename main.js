6a50047411628b2c733f0cda525ee50b0c5af243
Hunter Padgett <gizmokabanjo@gmail.com>
2023-02-21 20:00:41 -0500

    displaying drinks array to DOM via forEach

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 const input = document.querySelector("input");
 const searchBtn = document.querySelector("button");
-const drinkName = document.querySelector("h2");
-const drinkImg = document.querySelector("img");
-const drinkInstructions = document.querySelector("h3");
 const drinkContainer = document.querySelector(".drinkContainer");
 
-drinkName.textContent = "";
-drinkInstructions.textContent = "";
 searchBtn.addEventListener("click", searchCocktails);
 
 function searchCocktails() {
  const drink = input.value;
+ input.value = "";
+ drinkContainer.innerHTML = "";
 
  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
   .then((res) => res.json())
   .then((data) => {
-   console.log(data.drinks[0]);
-   drinkName.textContent = data.drinks[0].strDrink;
-   drinkImg.src = data.drinks[0].strDrinkThumb;
-   drinkInstructions.textContent = data.drinks[0].strInstructions;
+   const drinkArr = data.drinks;
+   drinkArr.forEach((drink) => {
+    renderCocktail(drink);
+   });
   })
   .catch((err) => console.log(err));
+}
+
+function renderCocktail(data) {
+ const html = `
+  <div class='drink'>
+    <h2 class='drinkName'>${data.strDrink}</h2>
+    <img class='drinkImg' src="${data.strDrinkThumb}" alt="image of cocktail" />
+    <h3 class='drinkInstructions'>${data.strInstructions}</h3>
+  </div>
+  `;
+ drinkContainer.insertAdjacentHTML("beforeend", html);
 }
